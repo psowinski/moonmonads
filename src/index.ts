9cf82e20c5270c7b4af6e9c7c51c1b8ecb0475d0
Przemyslaw Sowinski <psowinski@users.noreply.github.com>
2018-03-06 16:00:15 +0100

    index file header

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/**
+* @module Moon monads
+* @author Przemyslaw Sowinski <psowinski@users.noreply.github.com>
+* @version 1.0.0
+* @license MIT
+*/
+
 export * from './option'
 export * from './result'
 export * from './either'
